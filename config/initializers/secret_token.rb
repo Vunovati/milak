22e4fc20635f78af7f2b7678971d4eb0d469c55e
ovca <ovca@livada.(none)>
2011-08-30 18:16:56 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milak::Application.config.secret_token = 'a2e3a246753eabfa379c909dcbf2f2a5c81fe8ff419f968b7fa51d64cbf77e9a24077b54205832f9bbe4209d5467b09c82e57c757f4538cd41e32cc2d3068214'
